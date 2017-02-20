9784bddf1dd60ab5f2791c7344659b83b44f4d86
AlekMabry <alekmabry24@gmail.com>
2017-02-20 17:40:11 +0530

    Separate files for separate classes, hopefully to make it easier to add more CV algorithms as 'modules' instead of writing them into the main file.

--- BlobDetection.h
+++ BlobDetection.h
@@ -1,13 +1,19 @@
-//
-// Created by alek-ubuntuparallel on 2/18/17.
-//
+#include "opencv2/opencv.hpp"
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <stdio.h>
+#include "opencv2/highgui/highgui.hpp"
 
 #ifndef ROBOCORE_BLOBDETECTION_H
 #define ROBOCORE_BLOBDETECTION_H
 
+using namespace cv;
+using namespace std;
 
 class BlobDetection {
-
+public:
+    static Mat blobDetect(Mat frame);
 };
 
 
--- roboCore.cpp
+++ roboCore.cpp
@@ -1,51 +1,8 @@
 #include "opencv2/opencv.hpp"
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <stdio.h>
-#include "opencv2/highgui/highgui.hpp"
+#include "BlobDetection.h"
 
 using namespace cv;
 using namespace std;
-
-class BlobDetect {
-public:
-    static Mat blobDetect(Mat frame) {
-        Mat edges;
-        Mat contoursFrame;
-        vector<vector<Point> > contours;
-        vector<Vec4i> hierarchy;
-        int largest_area = 0;
-        int largest_contour_index = 0;
-
-        GaussianBlur(frame, edges, Size(7, 7), 1.5, 1.5);
-        cvtColor(edges, edges, COLOR_BGR2HSV);
-        inRange(edges, Scalar(36, 152, 50), Scalar(113, 255, 255), edges);
-        findContours(edges.clone(), contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-        if (contours.size() > 0) {
-            double xPos;
-            double yPos;
-            vector<Moments> mu(contours.size());
-            vector<Point2f> mc(contours.size());
-            for (int i = 0; i < contours.size(); i++) {
-                mu[i] = moments(contours[i], false);
-                mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
-                xPos = mu[i].m10 / mu[i].m00;
-                yPos = mu[i].m01 / mu[i].m00;
-                double a = contourArea(contours[i], false);
-                if (largest_area < a) {
-                    largest_area = a;
-                    largest_contour_index = i;
-                }
-            }
-            circle(frame, mc[largest_contour_index], 4, Scalar(255, 0, 0), -1, 8, 0);
-            drawContours(frame, contours, largest_contour_index, Scalar(255, 0, 0), 1, 8, hierarchy);
-
-        }
-        return frame;
-    }
-};
 
 int main(int, char**)
 {
@@ -55,7 +12,7 @@ int main(int, char**)
 
     for(;;) {
         cap >> frame;
-        frame = BlobDetect::blobDetect(frame);
+        frame = BlobDetection::blobDetect(frame);
         imshow("edges", frame);
         if (waitKey(30) >= 0) break;
     }
